bb77ed395041e5f5411ebb05291e64d8e2f9d859
Illia Salohub <eramcheg@gmail.com>
2024-01-10 02:01:39 +0100

    Unnecessary files deleted

--- comission_generator/test.py
+++
@@ -1,8 +0,0 @@
-product_info = {"Namee":"c"}
-
-client_name = ""
-if 'Name' in product_info.keys():
-        client_name = product_info['Name']
-else:
-        client_name = "Cliente"
-print(client_name)
