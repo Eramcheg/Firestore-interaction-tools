d19715c97184623666dedb275905d0349a4b1fbb
Illia Salohub <eramcheg@gmail.com>
2024-02-26 15:42:32 +0100

    Backup creator

--- other_old_or_useful_programs/backup_creater.py
+++ other_old_or_useful_programs/backup_creater.py
@@ -1,14 +1,22 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate("G:\\FIles\\firebase\\key4_Terminal.json")
+from datetime import datetime
+import json
+cred = credentials.Certificate("G:\\FIles\\firebase\\key2.json")
 firebase_admin.initialize_app(cred)
-collection_ref = firestore.client().collection("Product")
+collection_ref = firestore.client().collection("item")
 
-query = collection_ref.stream()
+docs = collection_ref.stream()
 
-i = 0
-for doc in query:
-    i+=1
-    if(i%10==0):
-        print(i)
-print("Finished with i = " + str(i))
+# Prepare data for backup
+data_for_backup = [doc.to_dict() for doc in docs]
+
+# Generate filename with today's date
+today_date = datetime.now().strftime("%d.%m.%Y")
+filename = f"item_backup_{today_date}.json"
+
+# Write data to JSON file
+with open(filename, "w", encoding="utf-8") as file:
+    json.dump(data_for_backup, file, ensure_ascii=False, indent=4)
+
+print(f"Backup created successfully: {filename}")
